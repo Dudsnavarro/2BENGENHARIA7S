23ee47c027c574f7938973b7671e25f4c150e4c8
Dudsnavarro <eduardo.dudunavarro@outlook.com>
2025-06-15 11:52:37 -0300

    SPRING SECURITY

---
+++ src/main/java/com/example/Trab_ArquiteturaDeSoftware/Repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.example.Trab_ArquiteturaDeSoftware.Repository;
+
+import com.example.Trab_ArquiteturaDeSoftware.Model.EnderecoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<EnderecoModel, Long> {
+}
